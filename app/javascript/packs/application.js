cccb848cda1c3151a46a004fbdfdc9f8fdc32612
D. Ip <ipkinghangdesmond@gmail.com>
2021-05-27 12:45:32 +0800

    made footer fancy, think im done

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -24,7 +24,7 @@ require("channels")
 
 // External imports
 import "bootstrap";
-import { show_email } from '../scripts/footer'
+import { show_email, hover_email } from '../scripts/footer'
 
 
 // Internal imports, e.g:
@@ -34,6 +34,7 @@ document.addEventListener('turbolinks:load', () => {
   // Call your functions here, e.g:
   // initSelect2();
   show_email();
+  hover_email();
 
 
 });
--- app/javascript/scripts/footer.js
+++ app/javascript/scripts/footer.js
@@ -2,15 +2,26 @@
 
 const show_email = () => {
   console.log("js script \"show_email\" running");
-  emailIcon = document.getElementById("email-icon");
-  emailAddress = document.getElementById("my-email");
-  emailIcon.addEventListener("toggle", () => {
-    emailAddress.toggleAttribute("hidden");
+  const emailAddress = document.getElementById("my-email");
+  const emailIcon = document.getElementById("email-icon");
+  emailIcon.addEventListener("click", () => {
+    console.log("click");
+    emailAddress.classList.toggle("hidden");
+  });
+}
+
+const hover_email = () => {
+  const emailIcon = document.getElementById("email-icon");
+  const emailAddress = document.getElementById("my-email");
+  const actualEmail = document.getElementById("email-address");
+  emailIcon.addEventListener("mouseover", ()=>{
+    console.log("hover");
+    emailAddress.classList.remove("hidden");
   });
 
 }
 
 
 
-export { show_email }
+export { show_email, hover_email}
 
